311272f40b636ca3795a65eba408e848d9e274e7
Kyle Burton <kyleb286@gmail.com>
2019-01-03 13:15:12 -0600

    update

--- B--FinalProject/B--FinalProject/Form1.cs
+++ B--FinalProject/B--FinalProject/Form1.cs
@@ -49,5 +49,66 @@ namespace B__FinalProject
 
         }
 
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+            public void lowPower()
+            {
+                int Out;
+                int FoulBall;
+                int Strike;
+                int Safe;
+                int minHit = 1;
+                int maxHit = 5;
+                int minPlay = 1;
+                int MaxPlay =
+
+
+
+                Random ranNumberGenerator = new Random();
+                int randomNumber;
+                randomNumber = ranNumberGenerator.Next(minHit, maxHit);
+
+
+
+                if (randomNumber == 1)
+                {
+                    var foul = new AerialView();
+                    foul.Show();
+                }
+
+
+            }
+        }
+
+        private void btnHigh_Click(object sender, EventArgs e)
+        {
+            public void lowPower()
+            {
+                int Out;
+                int FoulBall;
+                int Strike;
+                int Safe;
+                int minHit = 1;
+                int maxHit = 5;
+                int minPlay = 1;
+                int MaxPlay =
+
+
+
+                Random ranNumberGenerator = new Random();
+                int randomNumber;
+                randomNumber = ranNumberGenerator.Next(minHit, maxHit);
+
+
+
+                if (randomNumber == 1)
+                {
+                    var foul = new AerialView();
+                    foul.Show();
+                }
+
+
+            }
+        }
     }
 }
