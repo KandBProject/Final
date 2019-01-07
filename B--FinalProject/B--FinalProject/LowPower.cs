e82e2415345dc88b483103cbb171c35027e9cfa0
Brendan <camplese123@gmail.com>
2019-01-07 13:16:37 -0600

    Major work done

--- B--FinalProject/B--FinalProject/LowPower.cs
+++ B--FinalProject/B--FinalProject/LowPower.cs
@@ -8,37 +8,6 @@ namespace B__FinalProject
 {
     public class LowPower
     {
-        public static void lowPower()
-        {
-            
-                int Out;
-                int FoulBall;
-                int Strike;
-                int Safe;
-
-                int minPlay = 1;
-                int MaxPlay = 4;
-
-
-                int minHit = 1;
-                int maxHit = 5;
-                Random ranNumberG = new Random();
-                int randomNumber = ranNumberG.Next(minHit, maxHit);
-
-
-
-                if (randomNumber == 1)
-                {
-                    
-                }
-                else if (randomNumber == 2)
-                {
-                    
-                }
-
-
-            
-
-        }
+       
     }
 }
--- B--FinalProject/B--FinalProject/MedPower.cs
+++ B--FinalProject/B--FinalProject/MedPower.cs
@@ -8,34 +8,5 @@ namespace B__FinalProject
 {
     public class MedPower
     {
-        public static void medPower()
-        {
-            int Out;
-            int FoulBall;
-            int Strike;
-            int Safe;
-
-            int minPlay = 1;
-            int MaxPlay = 4;
-
-
-            int minHit = 1;
-            int maxHit = 7;
-            Random ranNumberG = new Random();
-            int randomNumber = ranNumberG.Next(minHit, maxHit);
-
-
-
-            if (randomNumber == 1)
-            {
-
-            }
-            else if (randomNumber == 2)
-            {
-
-            }
-
-
-        }
     }
 }
