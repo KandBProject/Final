92e22fd553ff9a73ab6ff52cbabd66f813e55e17
Brendan <camplese123@gmail.com>
2019-01-07 11:57:50 -0600

    Got classes working

--- B--FinalProject/B--FinalProject/HighPower.cs
+++ B--FinalProject/B--FinalProject/HighPower.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace B__FinalProject
 {
-    class HighPower
+    public class HighPower
     {
+        public static void highPower()
+        {
+            int Out;
+            int FoulBall;
+            int Strike;
+            int Safe;
+
+            int minPlay = 1;
+            int MaxPlay = 4;
+
+
+            int minHit = 1;
+            int maxHit = 4;
+            Random ranNumberG = new Random();
+            int randomNumber = ranNumberG.Next(minHit, maxHit);
+
+
+
+            if (randomNumber == 1)
+            {
+
+            }
+            else if (randomNumber == 2)
+            {
+
+            }
+
+
+        }
     }
 }
--- B--FinalProject/B--FinalProject/MedPower.cs
+++ B--FinalProject/B--FinalProject/MedPower.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace B__FinalProject
 {
-    class MedPower
+    public class MedPower
     {
+        public static void medPower()
+        {
+            int Out;
+            int FoulBall;
+            int Strike;
+            int Safe;
+
+            int minPlay = 1;
+            int MaxPlay = 4;
+
+
+            int minHit = 1;
+            int maxHit = 7;
+            Random ranNumberG = new Random();
+            int randomNumber = ranNumberG.Next(minHit, maxHit);
+
+
+
+            if (randomNumber == 1)
+            {
+
+            }
+            else if (randomNumber == 2)
+            {
+
+            }
+
+
+        }
     }
 }
